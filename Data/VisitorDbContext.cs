f8fe1de3a836076113be14776f5fb0a85845dd68
Darren Gorman <saintred-tech@hotmail.com>
2019-08-12 14:28:20 +1200

    Add project files.

---
+++ Data/VisitorDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Visitor_Management_System2019student.Models;
+
+namespace Visitor_Management_System2019student.Data
+{
+    public class visitorDBcontext : DbContext
+    {// here we add in the tables that we are using
+        public DbSet<StaffNames> StaffNames { get; set; }
+        public DbSet<Visitors> Visitors { get; set; }
+
+
+
+        public visitorDBcontext(DbContextOptions<visitorDBcontext> options)
+            : base(options)
+        {
+        }
+
+        public visitorDBcontext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Visitor_Management_System2019student.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
